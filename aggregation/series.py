cd9f5ef1b4fc0f2f2c524e766ac0ee60c21f38ca
Connect2waqas <waqasnadan972@gmail.com>
2026-02-28 02:14:18 +0500

    Add IPL performance data for Kohli and subscriber statistics
    - Created a new CSV file `kohli_ipl.csv` containing match numbers and runs scored by Virat Kohli in IPL.
    - Added a new CSV file `subs.csv` with subscriber gain statistics.
    - Updated `series.py` to read the new CSV files and print the first few entries for verification.

--- aggregation/series.py
+++ aggregation/series.py
@@ -1,19 +1,19 @@
+from numpy._core.fromnumeric import squeeze
 import numpy as np
 import pandas as pd
+subscriber = pd.read_csv("subs.csv").squeeze()
+koli = pd.read_csv("kohli_ipl.csv",index_col="match_no").squeeze()
+movie = pd.read_csv("bollywood.csv",index_col="movie").squeeze()
 
-marks =  [45,85,94,50]
-subject = ["English","Data Science","maths","science"]
-df  = pd.Series(marks,index=subject,name = "Marks of Waqas")
 
+print(type(subscriber.head()))
+print(type(koli.head()))
+print(type(movie.head()))
 
-def attributes_of_series(df):
-    # size = type(df.size)
-    # name = type(df.name)
-    # data_type = type(df.dtype)
-    # unique = type(df.is_unique)
-    # index_values = type(df.index)
-    # values = type(df.values)
-    print(df.index)
-    print(locals())
-
-attributes_of_series(df)
+def methods():
+  print(subscriber.head())
+  print(koli.head())
+  print(movie.head())
+  print(movie.sample()) # it select data randomly. if we pass number than it can give us the that number of value in return. by defult it give one.
+  
+methods()
